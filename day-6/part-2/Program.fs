30fbdae2ea7ab4e4561b0a2fa19acd34831b6bcb
Matt Bennett <matt.bennett91@gmail.com>
2020-12-12 14:24:37 +0100

    day 6 part 2

---
+++ day-6/part-2/Program.fs
@@ -0,0 +1,44 @@
+// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+
+open System.IO
+
+let readLines (filePath:string) = seq {
+    use sr = new StreamReader (filePath)
+    while not sr.EndOfStream do
+        yield sr.ReadLine ()
+}
+
+let inputFold input inputLine = 
+    match inputLine with
+    | "" -> List.empty :: input
+    | line -> match input with
+              | [] -> List.empty
+              | headInput :: restInput -> (line :: headInput) :: restInput
+
+let multiLineInput input 
+    = List.fold inputFold [ List.empty ] input |> List.rev
+
+[<EntryPoint>]
+let main argv =
+    let input = readLines "input.txt"
+                |> Seq.toList
+                |> multiLineInput
+                |> List.map ((fun x -> (List.length x, List.collect Seq.toList x))
+                             >> fun (x, y) -> (x, List.groupBy id y)
+                             >> fun (x, y) -> (x, List.map (fun(letter, answers) -> (letter, List.length answers)) y)
+                             >> fun (x, y) -> y |> List.filter (fun (_, answers) -> x = answers) |> List.length)
+                // |> List.iter (printfn "%A")
+                |> List.sum
+                |> printfn "%d"
+    // input 
+    //       |> List.rev
+    //       |> List.map (List.collect (fun x -> ["a";"b"]))
+    //       |> List.map (fun passport -> (validatePassport passport, passport, List.length passport))
+    //     //   |> List.iter (printfn "%A")
+    //       |> List.filter (fun x -> match x with | (valid, _, _) -> valid)
+          
+    //       |> List.length
+    //       |> printfn "%A"
+
+
+    0 // return an integer exit code
